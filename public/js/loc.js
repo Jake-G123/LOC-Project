c774921f2fe74c1f5dedd4c39a741ac134d898ae
Jonus <joclapshaw@gmail.com>
2025-10-18 22:21:32 -0700

    implementing things to test getting the options to show current info.

---
+++ public/js/loc.js
@@ -0,0 +1,3 @@
+//All the arrays with the info for each class
+var divisionInfo = ["Division", "Stipend Unit/Academic Program", "Contact: Division Chair", "Contact: Dean", "Contact: LOC rep", "PEN Contact"]
+var fineArts = ["Music", "Paul Metevier", "Christie Gilliland", "Monica Bowen", "Liz Peterson"]
