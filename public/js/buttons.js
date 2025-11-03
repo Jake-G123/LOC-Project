9b43767094d6ac7a0fb68a2e20bfa76946db8c0a
Jake-G123 <jakelgerken@gmail.com>
2025-11-02 22:25:16 -0800

    Deleted unused button.js and corrected some form labels

--- public/js/buttons.js
+++
@@ -1,9 +0,0 @@
-/*
-    This code is for the buttons to show and disappear when changes are made
-
-    work in progress not implemented yet.
-
-    TODO: If you want to try and figure it out you can I don't know if its entirely neccessary
-*/
-
-// This is now empty had to implement it into the loc.js. Remaking all the same variables would not be neccessary and increase run time
